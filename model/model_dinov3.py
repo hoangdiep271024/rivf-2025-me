c758f45d42bf26d47de84c0f2e1b15ed0329cf66
diepnh-0622 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-10 12:50:55 +0700

    Update model_dinov3.py

--- model/model_dinov3.py
+++ model/model_dinov3.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from transformers import AutoModel
 
 class DinoV3Classifier(nn.Module):
-    def __init__(self, num_classes: int, pretrained_model_name: str = "facebook/dinov3-vitl16-pretrain-lvd1689m"):
+    def __init__(self, num_classes: int, pretrained_model_name: str = "facebook/dinov3-vitb16-pretrain-lvd1689m"):
         super().__init__()
         self.backbone = AutoModel.from_pretrained(pretrained_model_name)
         self.classifier = nn.Linear(self.backbone.config.hidden_size, num_classes)
