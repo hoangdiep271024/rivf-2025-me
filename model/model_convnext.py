4274caa987822a405a062f88773fbdf886940471
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-10 20:54:00 +0700

    convnext

--- model/model_convnext.py
+++ model/model_convnext.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 def build_model(num_classes: int, pretrained: bool = True):
     model = timm.create_model(
-        "convnextv2_tiny",
+        "convnextv2_atto.fcmae_ft_in1k",
         pretrained=pretrained,
         num_classes=num_classes,       
     )
