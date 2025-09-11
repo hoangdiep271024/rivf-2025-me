14695ce4a4e53ec7f24f00ebfaf28720ebf9cf96
diepnh-0622 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-11 10:14:57 +0700

    Update model_convnext.py

--- model/model_convnext.py
+++ model/model_convnext.py
@@ -1,4 +1,4 @@
-# model.py
+# model_convnext.py
 from transformers import ConvNextForImageClassification
 import torch.nn as nn
 
@@ -7,17 +7,11 @@ MODEL_NAME = "facebook/convnext-large-224-22k-1k"
 def build_model(num_classes: int):
     """
     Build a pretrained ConvNeXt model for image classification.
-
-    Args:
-        num_classes (int): Number of output classes.
-
-    Returns:
-        model: ConvNeXtForImageClassification instance
     """
     # Load pretrained ConvNeXt
     model = ConvNextForImageClassification.from_pretrained(MODEL_NAME)
     
     # Reset classifier to match desired num_classes
-    model.classifier[2] = nn.Linear(model.classifier[2].in_features, num_classes)
+    model.classifier = nn.Linear(model.classifier.in_features, num_classes)
 
     return model
