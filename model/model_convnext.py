89b93602247bfbbd11e0d13c03108e780c5824a6
diepnh-0622 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-11 11:41:08 +0700

    Update model_convnext.py

--- model/model_convnext.py
+++ model/model_convnext.py
@@ -4,17 +4,23 @@ import torch.nn as nn
 
 MODEL_NAME = "vit_small_patch16_224.augreg_in21k_ft_in1k"
 
-def build_model(num_classes: int, hidden_dim: int = 512, dropout: float = 0.3):
-    model = timm.create_model(MODEL_NAME, pretrained=True)
-    in_features = model.get_classifier().in_features
-    
-    # Thay classifier mạnh hơn: Linear -> ReLU -> Dropout -> Linear
-    model.reset_classifier(0)
-    model.classifier = nn.Sequential(
-        nn.Linear(in_features, hidden_dim),
-        nn.ReLU(),
-        nn.Dropout(dropout),
-        nn.Linear(hidden_dim, num_classes)
-    )
-    
-    return model
+class ViTWrapper(nn.Module):
+    def __init__(self, num_classes=5, hidden_dim=512, dropout=0.3):
+        super().__init__()
+        self.model = timm.create_model(MODEL_NAME, pretrained=True)
+        in_features = self.model.get_classifier().in_features
+        
+        # Linear -> ReLU -> Dropout -> Linear, vẫn trả về size=num_classes
+        self.model.reset_classifier(0)
+        self.model.classifier = nn.Sequential(
+            nn.Linear(in_features, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, num_classes)
+        )
+        
+    def forward(self, x):
+        return self.model(x)  # trả về logits [batch, num_classes]
+
+def build_model(num_classes=5):
+    return ViTWrapper(num_classes)
