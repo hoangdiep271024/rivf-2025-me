95ba5977b672167a73ce77392c0026fb0a6addd6
diepnh-0622 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-11 11:18:02 +0700

    Update model_convnext.py

--- model/model_convnext.py
+++ model/model_convnext.py
@@ -2,11 +2,11 @@
 import timm
 import torch.nn as nn
 
-MODEL_NAME = "convnextv2_tiny.fcmae_ft_in22k_in1k"
+MODEL_NAME = "vit_base_patch16_224"
 
 def build_model(num_classes: int):
     # Load pretrained ConvNeXtV2
-    model = timm.create_model(MODEL_NAME, pretrained=False)
+    model = timm.create_model(MODEL_NAME, pretrained=True)
     
     # Reset classifier về đúng số lớp, 1 Linear cuối cùng
     model.reset_classifier(num_classes)
