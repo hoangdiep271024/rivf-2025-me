ff7507469961cbece1ab61f231152bb1f960d538
diepnh-0622 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-11 11:37:41 +0700

    Update model_convnext.py

--- model/model_convnext.py
+++ model/model_convnext.py
@@ -4,11 +4,17 @@ import torch.nn as nn
 
 MODEL_NAME = "vit_small_patch16_224.augreg_in21k_ft_in1k"
 
-def build_model(num_classes: int):
-    # Load pretrained ConvNeXtV2
+def build_model(num_classes: int, hidden_dim: int = 512, dropout: float = 0.3):
     model = timm.create_model(MODEL_NAME, pretrained=True)
+    in_features = model.get_classifier().in_features
     
-    # Reset classifier về đúng số lớp, 1 Linear cuối cùng
-    model.reset_classifier(num_classes)
+    # Thay classifier mạnh hơn: Linear -> ReLU -> Dropout -> Linear
+    model.reset_classifier(0)
+    model.classifier = nn.Sequential(
+        nn.Linear(in_features, hidden_dim),
+        nn.ReLU(),
+        nn.Dropout(dropout),
+        nn.Linear(hidden_dim, num_classes)
+    )
     
     return model
