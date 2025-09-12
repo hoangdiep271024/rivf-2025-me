324fdf9b87fc61b3c7061abd1379d7962e962008
diepnh-0622 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-13 00:37:22 +0700

    Update model_vgg.py

--- model/model_vgg.py
+++ model/model_vgg.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 def build_model(num_classes: int, pretrained: bool = True):
     model = timm.create_model(
-        "vgg13_bn",
+        "densenet201.tv_in1k",
         pretrained=pretrained,
         num_classes=num_classes,       
     )
