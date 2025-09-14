45dfa0591b5b4cf40a92f57c6bed49f8f8dabd2d
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-14 21:23:26 +0700

    diep

--- train_new.py
+++ train_new.py
@@ -264,7 +264,7 @@ if __name__ == "__main__":
         cfg = Config(
             train_csv=str(base_dir / f"fold_{fold}/train_new.csv"),
             valid_csv=str(base_dir / f"fold_{fold}/valid.csv"),
-            images_train_dir="./media/CASMEV2_new/dynamic_images",
+            images_train_dir="./media/CASMEV2_mix/dynamic_images",
             images_test_dir= "./media/CASMEV2/dynamic_images",
             outdir=f"./artifacts/learnNetmodels/checkpoints/fold_{fold}",
             log_dir=f"./artifacts/learnNetmodels/logs/fold_{fold}",
