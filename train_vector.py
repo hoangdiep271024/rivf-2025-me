642b9fba974bc9815f244513f4df8dbfb87f1b44
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-15 22:42:33 +0700

    diep

--- train_vector.py
+++ train_vector.py
@@ -289,7 +289,7 @@ if __name__ == "__main__":
             batch_size=32,
             lr=2e-3,
             weight_decay=4e-5,
-            epochs=120,
+            epochs=100,
             seed=42,
             use_class_weights=True,
             balance_sampler=False,
