398290fecacfc7ec3c8310aaca7817b1f8f0d201
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-16 15:08:38 +0700

    Diep

--- model/model_resnet.py
+++ model/model_resnet.py
@@ -1,7 +1,7 @@
 import timm
 import torch.nn as nn
 
-MODEL_NAME = "tf_efficientnetv2_s.in21k_ft_in1k"
+MODEL_NAME = "resnet50.a1_in1k"
 
 def build_model(num_classes: int, pretrained: bool = True):
     """
@@ -9,7 +9,6 @@ def build_model(num_classes: int, pretrained: bool = True):
     """
     model_base = timm.create_model(MODEL_NAME, pretrained=pretrained)
 
-    # Thay head cuối bằng num_classes
     if hasattr(model_base, "classifier"):  # EfficientNet, MobileNet...
         in_features = model_base.classifier.in_features
         model_base.classifier = nn.Linear(in_features, num_classes)
