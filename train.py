a26ba6d314ac5d42c0e7c4be9b710941d0b24f49
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-16 15:16:18 +0700

    diep

--- train.py
+++ train.py
@@ -264,7 +264,7 @@ if __name__ == "__main__":
         cfg = Config(
             train_csv=str(base_dir / f"fold_{fold}/train.csv"),
             valid_csv=str(base_dir / f"fold_{fold}/valid.csv"),
-            images_dir="/workspace/rivf-2025-me/media/CASMEV2/dynamic_images",
+            images_dir="./media/CASMEV2/dynamic_images",
             outdir=f"./artifacts/learnNetmodels/checkpoints/fold_{fold}",
             log_dir=f"./artifacts/learnNetmodels/logs/fold_{fold}",
             grayscale=False,
