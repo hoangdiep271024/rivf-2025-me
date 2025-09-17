b4478ee11f93b84ee15ad64d9ef2eeb8df627924
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-17 20:26:31 +0700

    Diep

--- model/model_radiov3.py
+++ model/model_radiov3.py
@@ -4,13 +4,21 @@ import torch.nn as nn
 MODEL_NAME = "c-radio_v3-b"
 
 class CustomModel(nn.Module):
-    def __init__(self, num_classes: int, extra_dim: int = 0):
+    def __init__(self, num_classes: int, extra_dim: int = 0, pretrained: bool = True):
         super().__init__()
         # Backbone RADIO
-        self.model_base = torch.hub.load(
-            'NVlabs/RADIO', 'radio_model',
-            version=MODEL_NAME, progress=True, skip_validation=True
-        )
+        if pretrained:
+            self.model_base = torch.hub.load(
+                'NVlabs/RADIO', 'radio_model',
+                version=MODEL_NAME, progress=True, skip_validation=True
+            )
+        else:
+            # không load pretrained (random init)
+            self.model_base = torch.hub.load(
+                'NVlabs/RADIO', 'radio_model',
+                version=MODEL_NAME, progress=False, skip_validation=True,
+                pretrained=False
+            )
 
         self.extra_dim = extra_dim
         self.classifier = None
@@ -22,7 +30,6 @@ class CustomModel(nn.Module):
         feat = out[0] if isinstance(out, (tuple, list)) else out  # (B, C)
         C = feat.size(1)
 
-        # Khởi tạo động classifier và extra_proj đúng device/dtype
         if self.classifier is None:
             device, dtype = feat.device, feat.dtype
             if self.extra_dim > 0:
@@ -44,8 +51,8 @@ class CustomModel(nn.Module):
         return self.classifier(feat)
 
 
-def build_model(num_classes: int, extra_dim: int = 0):
+def build_model(num_classes: int, extra_dim: int = 0, pretrained: bool = True):
     """
     Build RADIO model + optional extra vector.
     """
-    return CustomModel(num_classes=num_classes, extra_dim=extra_dim)
+    return CustomModel(num_classes=num_classes, extra_dim=extra_dim, pretrained=pretrained)
