a6c73c459c61d8bd716bdf522b8b93ac1cd4f020
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2025-09-23 21:47:20 +0700

    diep

--- train_vector.py
+++ train_vector.py
@@ -12,7 +12,7 @@ from torch.utils.data import DataLoader, WeightedRandomSampler
 from torch.utils.tensorboard import SummaryWriter
 
 from data_vector import build_datasets_from_splits, compute_class_weights as compute_class_weights_from_data
-from model.model_siglipv2 import build_model
+from model.model_resnet_new import build_model
 
 
 
@@ -278,16 +278,16 @@ def main(cfg: Config):
 
 
 if __name__ == "__main__":
-    base_dir = Path("./artifacts/samm_split")
+    base_dir = Path("./artifacts/casme_split")
     for fold in range(1, 6): 
         print(f"\n===== Training Fold {fold}/5 =====")
         cfg = Config(
             train_csv=str(base_dir / f"fold_{fold}/train.csv"),
             valid_csv=str(base_dir / f"fold_{fold}/valid.csv"),
-            images_dir="./media/SAMM/dynamic_images",
+            images_dir="./media/CASMEV2/dynamic_images",
             outdir=f"./artifacts/learnNetmodels/checkpoints/fold_{fold}",
             log_dir=f"./artifacts/learnNetmodels/logs/fold_{fold}",
-            npy_dir="./SMIRK_vector/SAMM_SMIRK_gaussian_shape",
+            npy_dir="./SMIRK_vector/CASME_SMIRK_gaussian_shape",
             grayscale=False,
             input_size=224,
             num_workers=4,
