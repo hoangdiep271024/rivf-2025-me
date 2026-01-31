6684cf1b652414efe60eaf467759a7acd70256f1
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2026-02-01 00:11:53 +0700

    diep

--- model/model_radiov4.py
+++ model/model_radiov4.py
@@ -33,7 +33,7 @@ class CustomModel(nn.Module):
 
         with torch.no_grad():
             out = self.model_base(y)
-        print(out[0].shape())
+        print(out[0].shape)
 
         out = self.model_base(x)
         feat = out[0] if isinstance(out, (tuple, list)) else out  # (B, 2304)
