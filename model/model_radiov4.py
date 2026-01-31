d1625482810e99758f73e613a28326dc144eb78c
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2026-02-01 00:21:15 +0700

    diep

--- model/model_radiov4.py
+++ model/model_radiov4.py
@@ -30,11 +30,9 @@ class CustomModel(nn.Module):
 
     def forward(self, x, extra_vec=None):
         y = torch.randn(1, 3, 224, 224)
-
         with torch.no_grad():
             out = self.model_base(y)
         print(out[0].shape)
-        return 0
 
         out = self.model_base(x)
         feat = out[0] if isinstance(out, (tuple, list)) else out  # (B, 2304)
